923c9b21be9068ade53e74c6a81c288315be8def
Hosea Eyob <hosea.eyob@macbook-pro.lan>
2020-09-16 01:55:50 -0500

    home and about addition

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+    return HttpResponse('<h1>Welcome to Finch Collector!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About the Finch Collector</h1>')
