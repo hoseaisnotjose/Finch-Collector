53f8873614519688b4cbcf3a1424dc2bebb1ab21
Hosea Eyob <hosea.eyob@macbook-pro.lan>
2020-09-18 14:45:34 -0500

    feeding model update, delete error fix

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 # importing reverse function
 from django.urls import reverse
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
+
 # Create your models here.
 class  Finch(models.Model):
     name = models.CharField(max_length=100)
@@ -9,9 +15,26 @@ class  Finch(models.Model):
     description = models.TextField(max_length=250)
     age = models.IntegerField()
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
-# redirecting the newly created Finch to Finch Details
-def get_absolute_url(self):
-    return reverse('detail', kwargs={'finch_id': self.id})
+    # redirecting the newly created Finch to Finch Details
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
+
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        # add the 'choices' field option
+        choices=MEALS,
+        # we will set the default value for meal to be 'B' (for breakfast)
+        default=MEALS[0][0]
+        )
+    
+    # create a finch_id FK
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+
