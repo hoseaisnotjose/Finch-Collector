dcac4d897f9809b27144f35cf5b6970a441e4fbf
Hosea Eyob <hosea.eyob@macbook-pro.lan>
2020-09-18 03:30:06 -0500

    add to Finch List

--- finchcollector/urls.py
+++ finchcollector/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+# add the include function to the import
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # '' represents the root route
     path('', include('main_app.urls')),
 ]
--- main_app/models.py
+++ main_app/models.py
@@ -7,4 +7,4 @@ class  Finch(models.Model):
     description = models.TextField(max_length=250)
     age = models.IntegerField()
 
-    
+  
