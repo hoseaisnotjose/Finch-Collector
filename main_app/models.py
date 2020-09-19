7440890f5ff058bb5236af0a52eac9b97f77bde3
Hosea Eyob <hosea.eyob@macbook-pro.lan>
2020-09-19 15:07:08 -0500

    toy functionalities update

--- main_app/models.py
+++ main_app/models.py
@@ -14,8 +14,14 @@ class Toy(models.Model):
     name = models.CharField(max_length=50)
     color = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
+
 # Create your models here.
-class  Finch(models.Model):
+class Finch(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
@@ -25,14 +31,14 @@ class  Finch(models.Model):
 
     def __str__(self):
         return self.name
-    
-    def fed_for_today(self):
-        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
     # redirecting the newly created Finch to Finch Details
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
 
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+    
 class Feeding(models.Model):
     date = models.DateField('feeding date')
     meal = models.CharField(
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Finch
+from django.views.generic import ListView, DetailView
+from .models import Finch, Toy
 from .forms import FeedingForm
 
 
@@ -19,7 +20,6 @@ class FinchDelete(DeleteView):
     model = Finch
     success_url = '/finches/'
 
-
 # Define the home view
 def home(request):
     return HttpResponse('<h1>Welcome to Finch Collector!</h1>')
@@ -33,6 +33,8 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
+    # def toys the finch doesn't have
+    toys_finch_doesnt_have = Toy.objects.exclude(id__in = finch.toys.all().values_list('id'))
     feeding_form = FeedingForm()
     return render(request, 'finches/detail.html', {
         'finch': finch, 'feeding_form': feeding_form,
@@ -51,3 +53,30 @@ def add_feeding(request, finch_id):
         new_feeding.save()
     return redirect('detail', finch_id=finch_id)
 
+def assoc_toy(request, finch_id, toy_id):
+    # I can pass a toy's id instead of the whole object
+    Finch.objects.get(id=finch_id).toys.add(toy_id)
+    return redirect('detail', finch_id=finch_id)
+
+def unassoc_toy(request, finch_id, toy_id):
+# I can pass a toy's id instead of the whole object
+    Finch.objects.get(id=finch_id).toys.remove(toy_id)
+    return redirect('detail', finch_id=finch_id)
+    
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
