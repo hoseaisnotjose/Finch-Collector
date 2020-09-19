a6cae559fa59ce01c2b065112fe4b02637801da8
Hosea Eyob <hosea.eyob@macbook-pro.lan>
2020-09-18 19:12:25 -0500

    default 'todays date' display update

--- main_app/models.py
+++ main_app/models.py
@@ -37,4 +37,8 @@ class Feeding(models.Model):
     
     def __str__(self):
         return f"{self.get_meal_display()} on {self.date}"
+    
+    # the below ensures current date is displayed at the top by default
+    class Meta:
+        ordering = ['-date']
 
